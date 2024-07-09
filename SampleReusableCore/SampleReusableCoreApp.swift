e401afd10182b2e48582771c3e55ce706c8f9043
Volodymyr Chekyrta <volodymyr.chekyrta@raccoongang.com>
2024-07-09 18:02:58 +0300

    Initial Commit

---
+++ SampleReusableCore/SampleReusableCoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleReusableCoreApp.swift
+//  SampleReusableCore
+//
+//  Created by Volodymyr Chekyrta on 9.07.24.
+//
+
+import SwiftUI
+
+@main
+struct SampleReusableCoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
